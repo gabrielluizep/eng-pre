a39215be093188c60265dfa5780b6ba6ffa19c5b
gabrielluizep <gabrielluizep@hotmail.com>
2023-09-24 22:38:55 -0300

    continuado prova 2

--- prova-2/prova-2-8.typ
+++ prova-2/prova-2-8.typ
@@ -1,4 +1,4 @@
-#import "../typst-ifsc/template-article.typ": article
+#import "../typst-ifsc/templates/article.typ": article
 
 #show: doc => article(
   title: "Prova 2",
@@ -15,20 +15,64 @@ independentemente. Sejam
 
 $ X = U_1 + U_2 + U_3 \ Y = U_1 + U_2 - U_3 $
 
-#enum(numbering: "(a)")
+#set enum(numbering: "(a)")
 
 + Determine a PMF conjunta de $X$ e $Y$.
 + Determine e esboce as PMFs marginais de $X$ e $Y$.
 + Determine e esboce as PMFs de $X$ dado que $Y = y$, para dois valores de $y in S_Y$ à
   sua escolha.
 
+#pagebreak();
+
 = Desenvolvimento
 
-== PMF conjunta de $X$
+== PMF conjunta de $X$ e de $Y$
 
-== PMF conjunta de $Y$
+#let values = ()
+
+#for i in (0, 1, 2) {
+  for j in (0, 1, 2) {
+    for k in (0, 1, 2) {
+      let x = i + j + k
+      let y = i + j - k
+
+      values.push([$#i$])
+      values.push([$#j$])
+      values.push([$#k$])
+      values.push([$1/27$])
+      values.push([$#x$])
+      values.push([$#y$])
+    }
+  }
+}
+
+// typstfmt::off
+#table(
+  columns: (1fr, 1fr, 1fr, 2fr, 2fr, 2fr),
+  inset: 10pt,
+  align: (horizon + center),
+  [$U_1$], [$U_2$], [$U_3$], [$Pr[U_1 and U_2 and U_3]$], [$X=U_1 + U_2 + U_3$], [$Y=U_1 + U_2 - U_3$],
+  ..values
+)
+// typstfmt::on
 
 == PMFs marginais de $X$ e $Y$
+// typstfmt::off
+#table(
+  columns: (1fr, 1fr, 1fr, 1fr, 1fr, 1fr, 1fr, 1fr, 1fr),
+  inset: 10pt,
+  align: (horizon + center),
+  [         ], [$y=-2$], [$y=-1$], [$y=0$ ], [$y=1$ ], [$y=2$], [$y=3$], [$y=4$], [$p_X (x)$],
+  [$x=0$    ], [$0$   ], [$0$   ], [$1/27$], [$0$   ], [$0$   ], [$0$   ], [$0$   ], [$1/27$],
+  [$x=1$    ], [$0$   ], [$1/27$], [$0$   ], [$2/27$], [$0$   ], [$0$   ], [$0$   ], [$3/27$],
+  [$x=2$    ], [$1/27$], [$0$   ], [$2/27$], [$0$   ], [$3/27$], [$0$   ], [$0$   ], [$6/27$],
+  [$x=3$    ], [$0$   ], [$2/27$], [$0$   ], [$3/27$], [$0$   ], [$2/27$], [$0$   ], [$7/27$],
+  [$x=4$    ], [$0$   ], [$0$   ], [$3/27$], [$0$   ], [$2/27$], [$0$   ], [$1/27$], [$6/27$],
+  [$x=5$    ], [$0$   ], [$0$   ], [$0$   ], [$2/27$], [$0$   ], [$1/27$], [$0$   ], [$3/27$],
+  [$x=6$    ], [$0$   ], [$0$   ], [$0$   ], [$0$   ], [$1/27$], [$0$   ], [$0$   ], [$1/27$],
+  [$p_Y (y)$], [$1/27$], [$3/27$], [$6/27$], [$7/27$], [$6/27$], [$3/27$], [$1/27$], [$1$   ],
+)
+// typstfmt::on
 
 === Esboços das PMFs marginais de $X$ e $Y$
 
--- prova-2/questao-8.m
+++ prova-2/questao-8.m
@@ -1,89 +1,92 @@
-lose all; clear all; clc;
+close all; clear all; clc;
+pkg load statistics;
 
-N = 10^5;
+N = 1e5;
 
-X = zeros(1, N);
-Y = zeros(1, N);
+U1 = randi([0 2], 1, N);
+U2 = randi([0 2], 1, N);
+U3 = randi([0 2], 1, N);
 
-% Gerando o X e o Y
-for i = 1 : N
-  A = [0 1 2];
-  
-  idx = randi([1 3]);
-  U1 = A(idx);          % Descobrindo se o U1 está em 0, 1 ou 2
-  
-  idx = randi([1 3]);
-  U2 = A(idx);          % Descobrindo se o U2 está em 0, 1 ou 2
-  
-  idx = randi([1 3]);
-  U3 = A(idx);          % Descobrindo se o U3 está em 0, 1 ou 2
-  
-  X(i) = U1 + U2 + U3;  % Definicao do X
-  Y(i) = U1 + U2 - U3;  % Definicao do Y
+X = U1 + U2 + U3;
+Y = U1 + U2 - U3;
+
+x = [0 1 2 3 4 5 6];
+y = [-2 -1 0 1 2 3 4];
+
+
+% Conjunta
+
+pmfXY_sim = hist3([X' Y'], {x, y}) / N
+pmfXY_teo = [   0      0   1/27      0      0      0      0; 
+                0   1/27      0   2/27      0      0      0;
+             1/27      0   2/27      0   3/27      0      0;
+                0   2/27      0   3/27      0   2/27      0;
+                0      0   3/27      0   2/27      0   1/27;
+                0      0      0   2/27      0   1/27      0;
+                0      0      0      0   1/27      0      0]
+
+% Marginais
+
+pmfX_sim = hist(X, x) / N
+pmfX_teo = [1/27 3/27 6/27 7/27 6/27 3/27 1/27]
+
+pmfY_sim = hist(Y, y) / N
+pmfY_teo = [1/27 3/27 6/27 7/27 6/27 3/27 1/27]
+
+% Condicionais
+
+pmfX_condY_sim = zeros(length(x), length(y));
+for i = 1:length(y)
+  XcondY = X(Y == y(i));
+  pmfX_condY_sim(i, :) = hist(XcondY, x) / sum(Y == y(i));
 end
 
-x = 0 : 1 : 6;          % O 'x' vai de 0 à 6
-y = -2 : 1 : 4;         % O 'y' vai de -2 à 4
+pmfX_condY_sim
+pmfX_condY_teo = [  0   0   1   0   0   0   0;
+                    0 1/3   0 2/3   0   0   0;
+                  1/6   0 2/6   0 3/6   0   0;
+                    0 2/7   0 3/7   0 2/7   0;
+                    0   0 3/6   0 2/6   0 1/6;
+                    0   0   0 2/3   0 1/3   0;
+                    0   0   0   0   1   0   0]
 
-histXY = zeros(7, 7);
-
-for i = 1 : 7
-  for j = 1 : 7
-    histXY(i, j) = sum(X == x(i) & Y == y(j));
-  end
+pmfY_condX_sim = zeros(length(y), length(x));
+for i = 1:length(x)
+  YcondX = Y(X == x(i));
+  pmfY_condX_sim(i, :) = hist(YcondX, y) / sum(X == x(i));
 end
 
-pmfXY_sim = histXY / N
-pmfXY_teo = [ 0    0   1/27  0    0    0    0;
-              0   1/27  0   2/27  0    0    0;
-             1/27  0   2/27  0   3/27  0    0;
-              0   2/27  0   3/27  0   2/27  0;
-              0    0   3/27  0   2/27  0   1/27;
-              0    0    0   2/27  0   1/27  0;
-              0    0    0    0   1/27  0    0]
-              
-% ----------------------------------------------------------------------------
+pmfY_condX_sim
+pmfY_condX_teo = [  0   0   1   0   0   0   0;
+                    0 1/3   0 2/3   0   0   0;
+                  1/6   0 2/6   0 3/6   0   0;
+                    0 2/7   0 3/7   0 2/7   0;
+                    0   0 3/6   0 2/6   0 1/6;
+                    0   0   0 2/3   0 1/3   0;
+                    0   0   0   0   1   0   0]
 
-% b) PMFs marginais de X e Y
-
-pmfX_sim = hist(X, x) / N;
-pmfX_teo = [1/27 3/27 6/27 7/27 6/27 3/27 1/27];
-
-pmfY_sim = hist(Y, y) / N;
-pmfY_teo = pmfX_teo;
-
-figure;
-subplot(2, 2, 1); hold on; grid on;
+figure; hold on; grid on;
 bar(x, pmfX_sim, 'y');
-stem(x, pmfX_teo, 'b', 'LineWidth', 4);
+stem(x, pmfX_teo, 'b', 'LineWidth', 3);
 xlabel('x'); ylabel('p_X(x)');
 
-subplot(2, 2, 2); hold on; grid on;
+figure;
+for i = 1 : length(y)
+  subplot(3, 3, i); hold on; grid on;
+  bar(x, pmfX_condY_sim(i, :), 'y');
+  stem(x, pmfX_condY_teo(i, :), 'b', 'LineWidth', 4);
+  xlabel('x'); ylabel(sprintf('p_X(x | Y=%d)', y(i)));
+end
+
+figure; hold on; grid on;
 bar(y, pmfY_sim, 'y');
-stem(y, pmfY_teo, 'b', 'LineWidth', 4);
+stem(y, pmfY_teo, 'b', 'LineWidth', 3);
 xlabel('y'); ylabel('p_Y(y)');
 
-% ----------------------------------------------------------------------------
-
-% c) PMFs condicionais de X dado que Y = y, para y ∈ {-2, 1}
-
-pmfX_condY_sim = zeros(2, 7);
-
-XcondY = X(Y == -2);
-pmfX_condY_sim(1, :) = hist(XcondY, x) / sum(Y == -2);
-
-XcondY = X(Y == 1);
-pmfX_condY_sim(2, :) = hist(XcondY, x) / sum(Y == 1);
-
-pmfX_condY_teo = [0  0  1  0  0  0  0;
-                  0 2/7 0 3/7 0 2/7 0];
-                  
-subplot(2, 2, 3); hold on; grid on;
-bar(x, pmfX_condY_sim(1, :), 'y');
-stem(x, pmfX_condY_teo(1, :), 'b', 'LineWidth', 4);
-xlabel('x'); ylabel(sprintf('p_X(x | Y = -2)'));
-
-subplot(2, 2, 4); hold on; grid on;
-bar(x, pmfX_condY_sim(2, :), 'y');
-stem(x, pmfX_condY_teo(2, :), 'b', 'LineWidth', 4);
-xlabel('x'); ylabel(sprintf('p_X(x | Y = 1)'));
+figure;
+for i = 1 : length(x)
+  subplot(3, 3, i); hold on; grid on;
+  bar(y, pmfY_condX_sim(i, :), 'y');
+  stem(y, pmfY_condX_teo(i, :), 'b', 'LineWidth', 4);
+  xlabel('y'); ylabel(sprintf('p_Y(y | X=%d)', x(i)));
+end
